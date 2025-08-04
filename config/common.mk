bfe5fafcf1ada9958d997298ea19ab10283c31bc
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-04 21:09:27 +0530

    config: Extend eppe check for CUSTOM_BUILD
    Change-Id: I6cc7d1ae2aed7b3ef2e2b9d838cbdaca8e40dd73

--- config/common.mk
+++ config/common.mk
@@ -18,6 +18,12 @@ include vendor/custom/config/charger.mk
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.launcher.depth.widget=true
 
+# EPPE
+ifneq ($(TARGET_DISABLE_EPPE),true)
+# Require all requested packages to exist
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CUSTOM_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
+endif
+
 # Fonts
 include vendor/custom/config/fonts.mk
 
