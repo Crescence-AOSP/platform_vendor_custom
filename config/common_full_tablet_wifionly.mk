747263173a9e2e43122c0a34cda459bdb83c15ae
Lunia <lunia0251@proton.me>
2026-08-10 17:52:52 +0700

    vendor: Include Pixel GMS
    Change-Id: I6f4c0db7d2af5bf0bcc4087c5c5cc88b65fc55bb

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+=======
+WITH_GMS_COMMS_SUITE := false
+
+# Inherit common stuff
+$(call inherit-product, vendor/custom/config/common.mk)
+>>>>>>> 011aaf7 (vendor: Include Pixel GMS)
 
 # Inherit our common stuff.
 $(call inherit-product, vendor/custom/config/common.mk)
