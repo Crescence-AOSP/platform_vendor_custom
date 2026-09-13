PixelOS-AOSP/android_vendor_custom

roomservice: Use better search api to find devices
Change-Id: I1c18e36294fa07ef3c0427b17d5217bfbb7a7778
Signed-off-by: inferno0230 <mail@inferno0230.in>

build/tools/roomservice.py
@@ -50,7 +50,7 @@ if not depsonly:
 repositories = []
 
 if not depsonly:
-    api_url = "https://api.github.com/orgs/PixelOS-Devices/repos?per_page=100"
+    api_url = f"https://api.github.com/search/repositories?q={device}+in:name+org:PixelOS-Devices"
     try:
         with urllib.request.urlopen(api_url, timeout=10) as response:
             repos_json = json.loads(response.read().decode())
@@ -61,8 +61,8 @@ if not depsonly:
         print("Failed to parse return data from GitHub API")
         sys.exit(1)
     # Extract repository names
-    for repo in repos_json:
-        repositories.append(repo['name'])
+    for repo in repos_json['items']:
+        repositories.append(repo["name"])
 
 local_manifests = r'.repo/local_manifests'
 if not os.path.exists(local_manifests): os.makedirs(local_manifests)

Crescence-AOSP/platform_vendor_custom

build/tools/roomservice.py
@@ -51,7 +51,7 @@ if not depsonly:
 repositories = []
 
 if not depsonly:
-    api_url = "https://api.github.com/orgs/Crescence-Devices/repos?per_page=100"
+    api_url = f"https://api.github.com/search/repositories?q={device}+in:name+org:Crescence-Devices"
     try:
         with urllib.request.urlopen(api_url, timeout=10) as response:
             repos_json = json.loads(response.read().decode())
@@ -62,7 +62,7 @@ if not depsonly:
         print("Failed to parse return data from GitHub API")
         sys.exit(1)
     # Extract repository names
-    for repo in repos_json:
+    for repo in repos_json.get('items', []):
         repositories.append(repo['name'])
 
 local_manifests = r'.repo/local_manifests'